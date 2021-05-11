2684fe68e62b1ab2c102ab1d14f9a31ead2b6e8d
ga-fleury <gabrielfleury95@gmail.com>
2021-05-11 17:32:32 -0300

    Hello World 🌎
    Svelte Typescript using Sass and TalwindCSS working.

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Tailwind from "./Tailwind.svelte"
+</script>
+
+<style lang="scss">
+    main {
+        h1 {
+            font-size: 22rem;
+        }
+    }
+</style>
+
+<Tailwind />
+<main>
+    <h1 class="text-gray-400">The rest of our app will be here</h1>
+</main>
+
