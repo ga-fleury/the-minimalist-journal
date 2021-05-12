8a4417e5e74551436219b94e7873785fbc3fe0bf
ga-fleury <gabrielfleury95@gmail.com>
2021-05-12 11:27:33 -0300

    Start GSAP
    messing with gsap next

---
+++ src/components/AboutHero.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import JournalDay from "./JournalDay.svelte";
+</script>
+
+<main>
+    <div class="container mx-auto h-screenh grid grid-cols-12 grid-rows-1 gap-4">
+        <div class="col-span-6 my-auto ml-64">
+            <h1 class="text-right text-6xl font-bold my-4">
+                how it works
+            </h1>
+            <p class="text-right text-3xl font-regular">
+                track your goals and push yourself to form new habits.
+            </p>
+        </div>
+        <div class="col-span-6 my-auto mr-96 ml-10">
+            <JournalDay />
+        </div>
+    </div>
+</main>
